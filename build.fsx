b261b1c91fdc051fa8baf4975d1d63aa085e9d11
Alexander Groß <agross@therightstuff.de>
2019-03-13 15:04:21 +0100

    Basic MSBuild setup

--- build.fsx
+++ build.fsx
@@ -1,14 +1,33 @@
+open System
 #r "paket:
 nuget Fake.Core.Target
+nuget Fake.DotNet.MsBuild
 //"
 
 #load "./.fake/build.fsx/intellisense.fsx"
 
 open Fake.Core
+open Fake.DotNet
 
 Target.create "Build" (fun _ ->
   Trace.trace "Build target is running"
+
+  let buildMode = Environment.environVarOrDefault "buildMode" "Release"
+  let setParams (defaults:MSBuildParams) =
+    { defaults with
+        Verbosity = Some(Quiet)
+        Targets = ["Build"]
+        Properties =
+          [
+            "Optimize", "True"
+            "DebugSymbols", "True"
+            "Configuration", buildMode
+          ]
+    }
+  MSBuild.build setParams "./MySolution.sln"
 )
+
+/// Target.Create("Build", (_) => { Console.WriteLine("Build target ist running") })
 
 Target.create "Test" (fun _ ->
   Trace.trace "Test target is running"
