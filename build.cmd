453b0d6b0160a6ffd5767bc8467e6a1d911335b2
Alexander Groß <agross@therightstuff.de>
2019-03-15 10:35:44 +0100

    Conditionally install fake-cli

--- build.cmd
+++ build.cmd
@@ -1,6 +1,7 @@
 @echo off
 
-dotnet tool install --global fake-cli
+dotnet tool list --global | findstr fake >NUL 2>&1
+if errorlevel 1 dotnet tool install --global fake-cli
 
 rem Pass -t <target> to run a target including dependencies.
 rem Pass -s -t <target> to run a single target w/o dependencies.
